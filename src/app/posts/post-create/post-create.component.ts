17925e77104e6d7cf2e2dadb8e3182de15ee610b
xxlxep <xxlxep@gmail.com>
2019-01-15 19:23:25 +0300

    first frontend

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredContent = '';
+  enteredTitle = '';
+
+
+  constructor(public postsService: PostsService) {
+
+  }
+
+  newPost = 'No Content';
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+
+  }
+
+}
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Post} from '../post.model';
+import { PostsService} from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit {
+
+posts: Post[] = [];
+
+
+
+constructor (public postsService: PostsService) {}
+
+ngOnInit() {
+  this.posts = this.postsService.getPosts();
+}
+}
