987b3fa2dcf3b4a62cd6ac3ee3831be34ee77260
xxlxep <xxlxep@gmail.com>
2019-01-18 18:44:14 +0300

    Transforming Response Data

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
+
+
 const app = express();
+
+mongoose.connect("mongodb+srv://xxlxep:bkQqgWCFyK7Njcln@cluster0-rotdz.mongodb.net/node-angular?retryWrites=true", { useNewUrlParser: true })
+ .then(() => {
+   console.log('Connected to database!');
+ })
+ .catch(() => {
+  console.log('Connection failed!');
+ });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,7 +25,11 @@ res.setHeader('Access-Control-Allow-Methods', 'GET,POST, PATCH, DELETE, OPTIONS'
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   console.log(post);
   res.status(201).json({
     message: 'Post fetched successfully'
@@ -22,13 +38,13 @@ app.post('/api/posts', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    { id: 'asdasdasd', title: 'First server-side post', content: 'This is coming from the server'},
-    { id: 'fgfdfgd', title: 'Second server-side post', content: 'This is coming from the server too!!!'}
-  ];
-  res.status(200).json({
-    message: 'Post fetched succesfully!',
-    posts: posts
+  Post.find()
+   .then(documents =>{
+      res.status(200).json({
+      message: 'Post fetched succesfully!',
+      posts: documents
+   });
+
   });
 });
 
