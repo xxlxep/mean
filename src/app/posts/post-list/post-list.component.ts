d22fc466203fbeb4a5930614acf55414929e43f4
xxlxep <xxlxep@gmail.com>
2019-01-24 00:15:45 +0300

    Working pagination

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -15,8 +15,9 @@ export class PostListComponent implements OnInit, OnDestroy {
 
 posts: Post[] = [];
 isLoading = false;
-totalPosts = 10;
+totalPosts = 0;
 postsPerPage = 2;
+currentPage = 1;
 pageSizeOptions = [1, 2, 5, 10];
 private postSub: Subscription;
 
@@ -26,20 +27,28 @@ constructor (public postsService: PostsService) {}
 
 ngOnInit() {
   this.isLoading = true;
-  this.postsService.getPosts();
+  this.postsService.getPosts(this.postsPerPage, this.currentPage);
   this.postSub = this.postsService.getPostUpdateListener()
-  .subscribe((posts: Post[]) => {
+  .subscribe((postData: {posts: Post[] , postCount: number}) => {
     this.isLoading = false;
-    this.posts = posts;
+    this.totalPosts = postData.postCount;
+    this.posts = postData.posts;
   });
 }
 
 onChangedPage(pageData: PageEvent) {
-
+  this.isLoading = true;
+  this.currentPage = pageData.pageIndex + 1;
+  this.postsPerPage = pageData.pageSize;
+  this.postsService.getPosts(this.postsPerPage, this.currentPage);
 }
 
 onDelete(postId: string) {
-  this.postsService.deletePost(postId);
+  this.isLoading = true;
+  this.postsService.deletePost(postId)
+  .subscribe(() => {
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  });
 }
 
 ngOnDestroy() {
